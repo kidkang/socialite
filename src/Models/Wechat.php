a8651051f7631a95a8f30e0c6c8591bd5e543fe4
kidkang <723616880@qq.com>
2020-03-31 14:32:18 +0800

    task#1208 微信登录包含emoji表情，导致登陆失败，加上一个编码方式

--- src/Models/Wechat.php
+++ src/Models/Wechat.php
@@ -8,7 +8,13 @@ class Wechat extends Model
 {
     protected $table = 'socialite_wechat';
     protected $fillable = ['client_id','openid','unionid','nickname','sex','city','province','country','headimgurl'];
+    public function setNicknameAttribute($value){
+        $this->attributes['nickname'] = userTextEncode($value);
+    }
 
+    public function getNicknameAttribute(){
+        return userTextDecode($this->attributes['nickname']);
+    }
     public function socialites(){
         return $this->morphMany('Yjtec\Socialite\Models\Socialite','socialitetable');
     }
